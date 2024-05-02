7c946c76a195231e897bc5fd2aba874a1c17b6ea
nitish986 <104351815+nitish986@users.noreply.github.com>
2024-05-02 10:00:48 +0000

    operations

---
+++ .devcontainer/01_basics/04_operations.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+3);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2*3);
+
+// operations in javascript
+let str1 = "hello"
+let str2 = " rahul"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" +2); //when we put the number first it will concatenate the string and then add the number
+ console.log(1 + "2"); //when we put the number first it will add the number and then concatenate the string
+ console.log("1" +2+2); //when we put the string first it will concatenate the string and then add the number
+ console.log(1+2+"2"); //when we put the number first it will add the number and then concatenate the string
+
+ console.log(3+4*4 %5);
+ console.log(2+ "2" + 2); //when we put the number first it will concatenate the string and then add the number
+
+ console.log(+true); //it will convert the boolean to number
+ console.log(+""); //it will convert the boolean to number
+ console.log(+"");  //it will convert the boolean to number
+ console.log(+"");  //it will convert the boolean to number
+
+ let num1, num2, num3
+ num1=num2=num3=2+2
+ console.log(num1,num2, num3);
+
+ let gameCounter = 100 
+ gameCounter++;
+ console.log(gameCounter); 
+ // prefix operator and postfix operator.  (incremental operator)
+ 
