05b66f27a0b6b1e8b2cebba261ddcd383c908433
nitish986 <104351815+nitish986@users.noreply.github.com>
2024-05-02 10:14:54 +0000

    comparison

---
+++ .devcontainer/01_basics/05_comparison.js
@@ -0,0 +1,27 @@
+console.log(2 > 1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log(2==2);
+console.log(2===2);
+
+console.log(2!=2);
+console.log(2!==2);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0); 
+console.log(null == 0); 
+console.log(null === 0);
+console.log(null>= 0);
+// most of the cases we avoid the conversion of the above null and undefined ....just remember one thing that clean code matters.
+
+// == double equal is called loose check because it only checks the values  
+
+console.log("2" === 2);
+
+// === tripal euqal is called strict check because it checks the data type and the values 
