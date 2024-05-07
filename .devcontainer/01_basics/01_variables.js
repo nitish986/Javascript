b29dfbb507f52f3a724a8bf4bb6d03c31c9dbfd0
nitish986 <104351815+nitish986@users.noreply.github.com>
2024-05-07 15:31:05 +0530

    Update 01_variables.js

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -19,3 +19,47 @@ speciallly use const and let for constant value because var is outdated so whene
 
 console.log(accountId);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// name = "Rahul"
+// age = 24
+// console.log(name,age);
+// isFollow = false;
+
+// name = 25;
+// console.log(name);
+// _class = 25;
+// console.log(_class);
+
+let name = "Rahul";
+const Name = "Rahul";
+var nam = "Rahul";
+console.log(name, Name, nam);
+
+// always use let or const for variable declaration
+// never use var for variable declaration
+
+// let and const are block scoped
+// var is function scoped
+
+// let and const are not hoisted
+
+// var is hoisted
+
+// let and const are not redeclared
+
+// var is redeclared
+// let and const are not reinitialized
+
+// var is reinitialized
+const PI = 3.14;
+console.log(PI);
+let a;
+console.log(a);
+{
+    let a= 5
+    console.log(a);
+}
+{
+    let a=6
+    console.log(a);
+}
+// we can only change the value of const but not redeclare or reinitialize it for redeclaring it we need to use another block {}.
