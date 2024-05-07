b26c4107e77abab9029a2b934f37c44a4a72846b
nitish986 <104351815+nitish986@users.noreply.github.com>
2024-05-07 16:18:57 +0530

    Update datatypes.js

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -31,6 +31,99 @@ console.log(typeof null); // output= object
 console.log(typeof undefined); //output = undefined
 
 
+//***************************************** */
+
+let y =Symbol("hello")
+console.log(y);
+let x = Symbol("hello")
+console.log(x);
+// Symbol is a unique value
+
+// Symbol is not hoisted
+
+// Symbol is not redeclared
+
+// Symbol is not reinitialized
+
+// Symbol is not block scoped
+
+//objects are collection of values.
+
+//for making any object we oftenly use const and let in block{}
+
+const student = {
+    fullName :"rahul kumar",
+    age:20,
+    cgpa:7.4,
+
+    isPass:true
+};
+console.log(student);
+console.log(student["age"]);
+// for accessing the value of object we use dot notation or bracket notation
+student["age"]= student["age"]+1;
+console.log(student["age"]);
+// for changing the value of object we use dot notation or bracket notation
+
+// for adding the value of object we use dot notation or bracket notation
+// for deleting the value of object we use delete keyword
+student["fullName"] = "rahul yadav";
+console.log(student["fullName"]);
+delete student["fullName"];
+console.log(student["fullName"]);
+
+// for accessing the value of object we use dot notation or bracket notation
+// for changing the value of object we use dot notation or bracket notation
+student["fullName"]= "rahul";
+console.log(student["fullName"]);
+
+//Note:- we can update the value in let and const but we can't redeclare or reinitialize it.
+// we can only change the object key of the object but not the value of the object.
+
+//*********************************** */
+//practice questions
+
+//01.- create a consr object called "product" to store information shown in the picture.
+
+const product = {
+    name : "Parker Jotter Standard CT Ball Pen (Black)",
+    color : "black",
+    rating: "****",
+    Offer : "20%",
+    price : 270,
+    isAvailable : true,
+};
+
+console.log(product);
+console.log(typeof (product));
+
+//********************* */
+//02:- create an object called profile to store information shown in the figure.
+
+const profile = {
+    name : "@Zerry2142",
+    followers : 600000,
+
+
+    following : 4,
+
+    posts : 198,
+
+    isVerified : true,
+    isFollow : true,
+    isPrivate : false,
+
+    isLive : false,
+
+    isStory : true,
+    bio : "Entrepreneur Apna College To educate someone is the highest privilage"
+
+    
+};
+console.log(profile);
+
+
+
 
 
 
